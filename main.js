7d83cbb0bffa80215914b477b3744ddbace9de12
matias4205 <matiasperezpc@hotmail.com>
2020-09-27 00:27:45 -0300

    Media links added

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const graphQlSVG = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 31.998 35.952">
-<g id="graphql" transform="translate(-25.741 -4.2)">
-  <path id="Unión_1" data-name="Unión 1" d="M2339.537,146.948a3.157,3.157,0,0,1,.129-.906l-7.393-4.267a3.2,3.2,0,1,1-3.918-4.99,3.192,3.192,0,0,1,.844-.338v-8.54a3.205,3.205,0,1,1,2.348-5.895,3.116,3.116,0,0,1,.725.572l7.395-4.269a3.206,3.206,0,1,1,6.279-.911,3.22,3.22,0,0,1-.133.916l7.393,4.268a3.166,3.166,0,0,1,.727-.575,3.206,3.206,0,1,1,2.352,5.895v8.54a3.206,3.206,0,1,1-2.359,5.892,3.251,3.251,0,0,1-.795-.643l-7.346,4.242a3.252,3.252,0,0,1,.162,1.01,3.2,3.2,0,1,1-6.408,0Zm5.572-2.159,7.363-4.251c-.023-.069-.041-.139-.061-.21h-19.344c-.01.042-.023.083-.033.124l7.4,4.271a3.208,3.208,0,0,1,4.678.065Zm7.3-5.985a3.19,3.19,0,0,1,.9-1.564l-9.676-16.759a3.227,3.227,0,0,1-1.785,0l-9.68,16.762a3.158,3.158,0,0,1,.563.715,3.2,3.2,0,0,1,.338.845Zm-21.561-2.32,9.676-16.763c-.031-.03-.059-.06-.09-.091l-7.408,4.277a3.2,3.2,0,0,1-2.3,3.991v8.551C2330.764,136.462,2330.809,136.473,2330.85,136.485Zm23.785,0c.041-.012.082-.023.125-.033V127.9a3.2,3.2,0,0,1-2.309-3.986l-7.41-4.278c-.029.029-.057.057-.086.085Z" transform="translate(-2301 -110)" fill="#e10098"/>
-</g>
-</svg>`;
+  <g id="graphql" transform="translate(-25.741 -4.2)">
+    <path id="Unión_1" data-name="Unión 1" d="M2339.537,146.948a3.157,3.157,0,0,1,.129-.906l-7.393-4.267a3.2,3.2,0,1,1-3.918-4.99,3.192,3.192,0,0,1,.844-.338v-8.54a3.205,3.205,0,1,1,2.348-5.895,3.116,3.116,0,0,1,.725.572l7.395-4.269a3.206,3.206,0,1,1,6.279-.911,3.22,3.22,0,0,1-.133.916l7.393,4.268a3.166,3.166,0,0,1,.727-.575,3.206,3.206,0,1,1,2.352,5.895v8.54a3.206,3.206,0,1,1-2.359,5.892,3.251,3.251,0,0,1-.795-.643l-7.346,4.242a3.252,3.252,0,0,1,.162,1.01,3.2,3.2,0,1,1-6.408,0Zm5.572-2.159,7.363-4.251c-.023-.069-.041-.139-.061-.21h-19.344c-.01.042-.023.083-.033.124l7.4,4.271a3.208,3.208,0,0,1,4.678.065Zm7.3-5.985a3.19,3.19,0,0,1,.9-1.564l-9.676-16.759a3.227,3.227,0,0,1-1.785,0l-9.68,16.762a3.158,3.158,0,0,1,.563.715,3.2,3.2,0,0,1,.338.845Zm-21.561-2.32,9.676-16.763c-.031-.03-.059-.06-.09-.091l-7.408,4.277a3.2,3.2,0,0,1-2.3,3.991v8.551C2330.764,136.462,2330.809,136.473,2330.85,136.485Zm23.785,0c.041-.012.082-.023.125-.033V127.9a3.2,3.2,0,0,1-2.309-3.986l-7.41-4.278c-.029.029-.057.057-.086.085Z" transform="translate(-2301 -110)" fill="#e10098"/>
+  </g>
+</svg>`
 
 const gatsbySVG = `<svg id="gatsby" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
 <path id="Trazado_5" data-name="Trazado 5" d="M16,0A16,16,0,1,0,32,16,16,16,0,0,0,16,0ZM3.438,16.167l12.4,12.4A12.563,12.563,0,0,1,3.438,16.167ZM18.812,28.248,3.752,13.188A12.566,12.566,0,0,1,26.158,8.606l-1.74,1.535A10.259,10.259,0,0,0,6.354,12.508L19.492,25.646a10.278,10.278,0,0,0,6.466-7.185H20.513V16h8.051A12.569,12.569,0,0,1,18.812,28.248Z" fill="#744c9e"/>
@@ -15,6 +15,26 @@ const reactSVG = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 28.387
 const nextSVG = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 21.772">
 <path id="nextjs" d="M8.642,5.885h6.792v.541H9.265V10.5h5.8v.541h-5.8v4.47H15.5v.541H8.642Zm7.4,0h.722l3.2,4.47,3.269-4.47L27.676.207l-7.3,10.619,3.764,5.225h-.75L19.962,11.3l-3.438,4.755h-.736l3.792-5.225-3.537-4.94Zm8.363.541V5.885h7.74v.541H28.579v9.624h-.623V6.427H24.405ZM.145,5.885H.923L11.655,21.979,7.22,16.051.8,6.654l-.028,9.4H.145Z" transform="translate(-0.145 -0.207)"/>
 </svg>`
+
+/* Media SVGs */
+const mediaSVGs = {
+  twitter: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 25.99">
+  <path id="twitter-icon" d="M28.711,9.858c.02.284.02.569.02.853,0,8.67-6.6,18.66-18.66,18.66A18.533,18.533,0,0,1,0,26.427a13.567,13.567,0,0,0,1.584.081,13.135,13.135,0,0,0,8.142-2.8,6.57,6.57,0,0,1-6.132-4.548,8.271,8.271,0,0,0,1.239.1,6.936,6.936,0,0,0,1.726-.223A6.559,6.559,0,0,1,1.3,12.6v-.081a6.6,6.6,0,0,0,2.964.832,6.568,6.568,0,0,1-2.03-8.772,18.642,18.642,0,0,0,13.523,6.863,7.4,7.4,0,0,1-.162-1.5,6.565,6.565,0,0,1,11.35-4.487,12.912,12.912,0,0,0,4.162-1.584,6.541,6.541,0,0,1-2.883,3.614A13.148,13.148,0,0,0,32,6.467a14.1,14.1,0,0,1-3.289,3.391Z" transform="translate(0 -3.381)"/>
+  </svg>`,
+
+  facebook: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+  <path id="facebook-icon" d="M28.571,2.25H3.429A3.429,3.429,0,0,0,0,5.679V30.821A3.429,3.429,0,0,0,3.429,34.25h9.8V23.371h-4.5V18.25h4.5v-3.9c0-4.439,2.643-6.891,6.691-6.891a27.262,27.262,0,0,1,3.966.346v4.357H21.655a2.561,2.561,0,0,0-2.887,2.766V18.25h4.913L22.9,23.371H18.768V34.25h9.8A3.429,3.429,0,0,0,32,30.821V5.679A3.429,3.429,0,0,0,28.571,2.25Z" transform="translate(0 -2.25)"/>
+  </svg>`,
+
+  linkedIn: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 31.999">
+  <path id="linkedin-icon" d="M7.163,32H.529V10.636H7.163ZM3.842,7.721A3.86,3.86,0,1,1,7.684,3.843,3.875,3.875,0,0,1,3.842,7.721ZM31.993,32h-6.62V21.6c0-2.479-.05-5.657-3.449-5.657-3.449,0-3.978,2.693-3.978,5.479V32H11.319V10.636h6.363V13.55h.093a6.971,6.971,0,0,1,6.277-3.45C30.766,10.1,32,14.521,32,20.264V32Z" transform="translate(0 -0.001)"/>
+  </svg>`,
+
+  website: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+  <path id="link-icon" d="M20.413,11.587A9.5,9.5,0,0,1,20.436,25l-.022.023-4.2,4.2A9.5,9.5,0,0,1,2.778,15.787L5.1,13.468a1,1,0,0,1,1.706.663,11.517,11.517,0,0,0,.606,3.3,1.006,1.006,0,0,1-.236,1.038l-.818.818a4.5,4.5,0,1,0,6.323,6.4l4.2-4.2a4.5,4.5,0,0,0,0-6.364,4.682,4.682,0,0,0-.646-.536A1,1,0,0,1,15.8,13.8a2.489,2.489,0,0,1,.731-1.863l1.316-1.316a1,1,0,0,1,1.287-.108,9.53,9.53,0,0,1,1.283,1.075Zm8.808-8.809a9.511,9.511,0,0,0-13.435,0l-4.2,4.2L11.564,7a9.5,9.5,0,0,0,1.305,14.487,1,1,0,0,0,1.286-.108l1.316-1.316A2.489,2.489,0,0,0,16.2,18.2a1,1,0,0,0-.434-.788,4.682,4.682,0,0,1-.646-.536,4.5,4.5,0,0,1,0-6.364l4.2-4.2a4.5,4.5,0,1,1,6.323,6.4l-.818.818a1.006,1.006,0,0,0-.236,1.038,11.517,11.517,0,0,1,.606,3.3,1,1,0,0,0,1.706.663l2.319-2.319a9.51,9.51,0,0,0,0-13.435Z" transform="translate(0 0)"/>
+  </svg>`
+}
+
 
 class ExplainIt {
   normalizeStackNames = (stack) => stack.map((stackItem = '') => stackItem.toLowerCase())
@@ -34,10 +54,29 @@ class ExplainIt {
     }
   }
 
+  getMediaHTML = (mediaKey, value) => {
+    const wrapIntoAnchor = (svg) => `<a class="media__item" href="${value}">${svg}</a>`
+    console.log({ mediaKey, value });
+    return wrapIntoAnchor(mediaSVGs[mediaKey])
+  }
+
   renderStack = (stack = []) => this.normalizeStackNames(stack).reduce((prev, stackItem) => prev + this.getStackHTML(stackItem), '')
 
+  renderMedia = (media) => Object.keys(media)
+    .filter(mediaKey => Boolean(media[mediaKey]))
+    .reduce((prev, mediaKey) => prev + this.getMediaHTML(mediaKey, media[mediaKey]), '')
+    /* .forEach((mediaKey) => {
+      console.log({mediaKey, value: media[mediaKey]});
+    }) */
+
+  hasMedia = (media = {}) => Object.keys(media).some((mediaKey) => media[mediaKey])
+
   getWidgetHTML = ({
+    title,
+    shortDescription,
+    description,
     stack,
+    media,
   }) => `
     <div class="explainit">
       <div id="explainit__frame" class="explainit__frame">
@@ -45,11 +84,14 @@ class ExplainIt {
           <div class="header">
             <div class="header__overlay"></div>
             <div class="header__content">
-              <h1 class="title">Petgram</h1>
-              <p class="shortDescription">
-                Petgram es una aplicación realizada en el curso de React Avanzado en Platzi.
-                Es una aplicación similar a Instagram con el detalle que las imágenes que se muestran son de mascotas.
-              </p>
+              <h1 class="title">${title}</h1>
+              ${
+                shortDescription.length > 0 ? (`
+                  <p class="shortDescription">
+                    ${shortDescription}
+                  </p>
+                `) : ''
+              }
             </div>
           </div>
           <div class="content">
@@ -68,42 +110,29 @@ class ExplainIt {
                     </div>`
                 ) : ''
               }
-              <div class="card">
-                <div class="card__head">
-                  <h3>Description</h3>
-                </div>
-                <div class="card__content">
-                  <p>
-                    Petgram es una PWA(Progressive Web App) de aspecto similar a instagram donde podés encontrar fotos de
-                    mascotas. Este proyecto fue realizado en el curso de React Avanzado en Platzi.
-                  </p>
-                  <h3>¿Con que esta hecho?</h3>
-                  <ul>
-                    <li>
-                      ⚛️ React
-                    </li>
-                    <li>
-                      🚀 Apollo client (GraphQL)
-                    </li>
-                    <li>
-                      💅 Styled components
-                    </li>
-                    <li>
-                      🔍 Cypress
-                    </li>
-                  </ul>
-                  <br/>
-
-                  <h3>¿Que puedo hacer con Petgram?</h3>
-                  <p>
-                    Con Petgram podés crearte una cuenta y likear todas las publicaciones de gatitos que quieras 😉.
-                  </p>
-
-                  <br/>
-                  <h3>¿Dónde puedo verla?</h3>
-                  <p>Justo aca 👉 <a>https://petgram-seven-ochre.vercel.app/</a> </p>
-                </div>
-              </div>
+              ${
+                description.length > 0 ? (`
+                  <div class="card">
+                    <div class="card__head">
+                      <h3>Description</h3>
+                    </div>
+                    <div class="card__content">
+                      ${description}
+                    </div>
+                  </div>
+                `) : ''
+              }
+              ${
+                this.hasMedia(media) ? (`
+                  <div class="card">
+                    <div class="card__content">
+                      <ul class="media">
+                        ${this.renderMedia(media)}
+                      </ul>
+                    </div>
+                  </div>
+                `) : ''
+              }
             </div>
           </div>
         </div>
@@ -124,20 +153,42 @@ class ExplainIt {
     description = '',
     stack = [],
     rootElm = null,
+    media = {
+      website: null,
+      linkedIn: null,
+      twitter: null,
+      facebook: null,
+    }
   } = {}) {
     this.title = title;
     this.shortDescription = shortDescription;
     this.description = description;
     this.stack = stack;
     this.rootElm = rootElm || document.querySelector('body');
+    this.media = {
+      website: media.website,
+      linkedIn: media.linkedIn,
+      twitter: media.twitter,
+      facebook: media.facebook,
+    };
 
     this.initWidget();
   }
 
   initWidget() {
     this.rootElm.innerHTML = this.getWidgetHTML({
+      title: this.title,
+      shortDescription: this.shortDescription,
+      description: this.description,
       stack: this.stack,
+      media: {
+        website: this.media.website,
+        linkedIn: this.media.linkedIn,
+        twitter: this.media.twitter,
+        facebook: this.media.facebook,
+      }
     });
+
     const trigger = document.getElementById('explainit__launcher')
     const content = document.getElementById('explainit__frame')
 
@@ -149,7 +200,42 @@ class ExplainIt {
 
 const explainit = new ExplainIt({
   title: 'Petgram',
-  shortDescription: 'Sarasear',
-  description: 'Salalalalala',
-  stack: ['graphQl', 'next', ],
+  shortDescription: 'Petgram es una aplicación realizada en el curso de React Avanzado en Platzi. Es una aplicación similar a Instagram con el detalle que las imágenes que se muestran son de mascotas.',
+  stack: ['graphql', 'react' ],
+  description: `
+    <p>
+      Petgram es una PWA(Progressive Web App) de aspecto similar a instagram donde podés encontrar fotos de
+      mascotas.
+    </p>
+    <h3>¿Con que esta hecho?</h3>
+    <ul>
+      <li>
+        ⚛️ React
+      </li>
+      <li>
+        🚀 Apollo client (GraphQL)
+      </li>
+      <li>
+        💅 Styled components
+      </li>
+      <li>
+        🔍 Cypress
+      </li>
+    </ul>
+    <br/>
+
+    <h3>¿Que puedo hacer con Petgram?</h3>
+    <p>
+      Con Petgram podés crearte una cuenta y likear todas las publicaciones de gatitos que quieras 😉.
+    </p>
+
+    <br/>
+    <h3>¿Dónde puedo verla?</h3>
+    <p>Justo aca 👉 <a href="https://petgram-seven-ochre.vercel.app">https://petgram-seven-ochre.vercel.app</a></p>
+  `,
+  media: {
+    website: 'https://mtprz.dev',
+    twitter: 'https://twitter.com/matiasperz_',
+    linkedIn: 'https://www.linkedin.com/in/matias-sebastian-perez-24012b180/'
+  },
 });
